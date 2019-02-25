ee5b82d72d2d28b8f0e276baa519e91427eed910
gzm916 <gzm916@my.utsa.edu>
2019-02-24 22:24:45 -0600

    Part 1 done

--- prog.c
+++ prog.c
@@ -72,9 +72,13 @@ int main(int argc, char* argv[])
             PCB->Reg[i] = PCB->ProcId;
             //printf("i = %d\n", PCB->Reg[i]);
         }
+
+        PCB->queueEnterClock = 0;
+        PCB->waitingTime = 0;
         //printf("blah %d\n", PCB->ProcPR);
         push(PCB);
         setTail();
+        //free(PCB);
 
         //printf("yo %d %d %d %d\n", scancount, PCB->ProcId, PCB->ProcPR, PCB->CPUburst); //debug
     }
@@ -86,6 +90,8 @@ int main(int argc, char* argv[])
     printf("Input File Name: %s\n", inputf);
     printf("CPU Scheduling Alg: %s\n", algo);
     
+    FIFO_Scheduling();
+
     return 0;
     
 }
@@ -128,5 +134,39 @@ void push(struct PCB_st *p)
 
 void FIFO_Scheduling()
 {
+    int i;
+    struct PCB_st *PCB = Head;
+
+    while(Head->next != NULL || Head == Tail)
+    {
+        for(i = 0; i < (sizeof(PCB->Reg)/sizeof(PCB->Reg[0])); i++)
+        {
+            CPUreg[i] = PCB->Reg[i];
+            CPUreg[i]++;
+            PCB->Reg[i] = CPUreg[i];
+        }
+        
+        PCB->waitingTime = PCB->waitingTime + CLOCK - PCB->queueEnterClock;
+        Total_waiting_time = Total_waiting_time + PCB->waitingTime;
+        CLOCK = CLOCK + PCB->CPUburst;
+        Total_turnaround_time = Total_turnaround_time + CLOCK;
+        Total_job = Total_job + 1;
+        
+        printf("Process %d is completed at %d ms\n", PCB->ProcId, CLOCK);
+        
+        if(Head == Tail)
+        {
+            free(Head);
+            break;
+        }
+
+        PCB = Head->next;
+        free(Head);
+        Head = PCB;
+        
+    }
     
+    printf("Average Waiting time = %2.2lf ms\n", ((double)Total_waiting_time/Total_job));
+    printf("Average Turnaround time = %2.2lf ms\n", ((double)Total_turnaround_time/Total_job));
+    printf("Throughput = %2.2lf jobs per ms\n", ((double)Total_job/CLOCK));
 }
